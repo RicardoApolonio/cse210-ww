fdeafc727dfe44b08b2ee8caa567faadf9ce0544
Ricardo Apolonio <greesiapolorian@gmail.com>
2026-05-29 12:41:42 -0400

    W04 Online Ordering completed

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address(
+            "123 Main St",
+            "New York",
+            "NY",
+            "USA"
+        );
+
+        Customer customer1 = new Customer(
+            "John Smith",
+            address1
+        );
+
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Laptop", "P100", 1200, 1));
+        order1.AddProduct(new Product("Mouse", "P200", 25, 2));
+
+        Address address2 = new Address(
+            "456 Queen St",
+            "Toronto",
+            "Ontario",
+            "Canada"
+        );
+
+        Customer customer2 = new Customer(
+            "Maria Lopez",
+            address2
+        );
+
+        Order order2 = new Order(customer2);
+
+        order2.AddProduct(new Product("Keyboard", "P300", 80, 1));
+        order2.AddProduct(new Product("Monitor", "P400", 300, 1));
+
+        Console.WriteLine("ORDER 1");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost()}");
+
+        Console.WriteLine();
+
+        Console.WriteLine("ORDER 2");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost()}");
     }
 }
